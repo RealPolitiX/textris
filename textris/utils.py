2ee7dd7ae391e592667b2a65ef01600b2ba0db09
RealPolitiX <xrpatrick@gmail.com>
2023-11-21 01:33:38 -0800

    Added utils.py

---
+++ textris/utils.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.colors as mplc
+
+
+class MidpointNormalize(mplc.Normalize):
+    
+    def __init__(self, vmin=None, vmax=None, midpoint=None, clip=False):
+        self.midpoint = midpoint
+        super().__init__(vmin, vmax, clip)
+
+    def __call__(self, value, clip=None):
+        # Ignoring masked values
+        
+        x, y = [self.vmin, self.midpoint, self.vmax], [0, 0.5, 1]
+        return np.ma.masked_array(np.interp(value, x, y))
